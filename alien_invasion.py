29e05faa44687f8e2c20a10893c493639c710619
LYLYLY7 <lylyly72020@163.com>
2024-09-10 21:54:21 +0800

    左右移动且一直按一直移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,7 @@ class AlienInvasion:
         pygame.init()
         # 计时
         self.clock = pygame.time.Clock()
+
         self.settings = Settings()
         # 游戏窗口大小
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
@@ -24,28 +25,39 @@ class AlienInvasion:
         """开始游戏的主循环"""
 
         while True:
+            # 监听键盘鼠标事件
             self._check_events()
+            # 更新船向右位置
+            self.ship.update()
+            # 重新渲染屏幕
             self._update_screen()
             # 循环尽量保证每秒运行60次
             self.clock.tick(60)
 
     def _check_events(self):
-        # 侦听键盘和⿏标事件
+        """侦听键盘和⿏标事件"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # 左右移动飞船 按下一直移动 松开停止
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # 向右移动飞船
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
-        self.screen.fill(self.settings.bg_color)
         """填充背景色"""
         self.screen.fill(self.settings.bg_color)
         """添加ship飞船"""
         self.ship.blitme()
+        """重新渲染"""
         pygame.display.flip()
 
 
--- ship.py
+++ ship.py
@@ -1,8 +1,10 @@
 import pygame
 
+
 class Ship:
     """管理飞船的类"""
     """ai_game引用AlienInvasion实例"""
+
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
@@ -15,6 +17,16 @@ class Ship:
 
         # 每艘新⻜船都放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
+        # 移动标志（飞船一开始不动）
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """向右移动飞船"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """在指定位置绘制⻜船"""
