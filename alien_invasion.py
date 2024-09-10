c2a74b772ac4d521a6282f1ba075347a0d5a443f
LYLYLY7 <lylyly72020@163.com>
2024-09-10 20:56:08 +0800

    新建settings.py 迁移屏幕默认设置

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -9,11 +10,10 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        # 绘制窗口宽1200 高800
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
         # 窗口标题
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -24,7 +24,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #  每次循环时重绘屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # 让最近绘制的屏幕可⻅，不断更新屏幕
             pygame.display.flip()
             # 循环尽量保证每秒运行60次
