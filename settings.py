af872e64fec7b170b040df33cf12c8ff96b3a24b
LYLYLY7 <lylyly72020@163.com>
2024-09-11 08:30:36 +0800

    更新飞船模型

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings:
         # 屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = 'black'
+        self.bg_color = (230, 230, 230)
         # 飞船的设置
         self.ship_speed = 1.5
