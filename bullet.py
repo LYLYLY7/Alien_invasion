12f6006fe0b11272929a037562d0ddd6e6c8b1bb
LYLYLY7 <lylyly72020@163.com>
2024-09-11 08:43:14 +0800

    向上移动子弹

--- bullet.py
+++ bullet.py
@@ -18,3 +18,14 @@ class Bullet(Sprite):
 
         # 存储用浮点数表示的子弹位置
         self.y = float(self.rect.y)
+
+    def update(self):
+        """向上移动子弹"""
+        # 更新子弹的准确位置
+        self.y -= self.settings.bullet_speed
+        # 更新表示子弹的rect的位置
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """在屏幕上绘制子弹"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
