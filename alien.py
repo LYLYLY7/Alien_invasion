cb06a5ee228a6df5b13fd41f4345608da8c22d63
LYLYLY7 <lylyly72020@163.com>
2024-09-11 09:15:12 +0800

    创建Alien类

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """表⽰单个外星⼈的类"""
+
+    def __init__(self, ai_game):
+        """初始化外星⼈并设置其起始位置"""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # 加载外星⼈图像并设置其 rect 属性
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # 每个外星⼈最初都在屏幕的左上角附近
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # 存储外星⼈的精确⽔平位置
+        self.x = float(self.rect.x)
